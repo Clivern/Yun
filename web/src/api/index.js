cd169bcbd1d3ab5b8e774933820b8346ab49e5ba
clivern <hello@clivern.com>
2025-11-17 23:07:06 +0100

    feat: Add users page

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -61,9 +61,10 @@ export const setupAPI = {
   checkInstalled: () => api.get('public/action/setup/status'),
 }
 
-// API endpoints for users C
+// API endpoints for users
 export const userAPI = {
-  getUsers: () => api.get('/users'),
+  getUsers: (params = {}) => api.get('/users', { params }),
+  getUser: (id) => api.get(`/users/${id}`),
   createUser: (data) => api.post('/users', data),
   updateUser: (id, data) => api.put(`/users/${id}`, data),
   deleteUser: (id) => api.delete(`/users/${id}`),
